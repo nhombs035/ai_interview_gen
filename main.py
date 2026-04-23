17aeddaacf3ab3854ca87d09aa3919016670ff95
Natalia Hombs <nhombs@purdue.edu>
2026-04-22 22:35:48 -0400

    Update formating and documentation of main front end

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from resume_parse import parse_resume
 from generator import to_json, generate_feedback, generate_questions
 
 
-## INITIALIZATION of set lists 
+######################################## GLOBAL LIST INITALIZATIONS ############################################
+
+# Standardized rubric grading areas to grade each question
 RUBRIC_AREAS = [
     "Problem Understanding",
     "Algorithm and Approach",
@@ -15,7 +17,7 @@ RUBRIC_AREAS = [
     "Communication Clarity"
 ]
 
-
+# Technical areas that questions are generated from 
 FOCUS_AREAS = [
     "Product Thinking",
     "Ownership",
@@ -29,6 +31,7 @@ FOCUS_AREAS = [
     "Collaboration"
 ]
 
+# Color maping for each focus area to easily recognize
 COLOR_MAP = {
     "Product Thinking": "#C86161",
     "Ownership": "#DA864F",
@@ -42,7 +45,7 @@ COLOR_MAP = {
     "Collaboration": "#D06C94"
 }
 
-## Custom tag/fake buttons for topic selection
+# Custom tag/fake buttons for topic selection to display under each question
 def focus_tags_disp(focus_list):
     focus_list = [f.strip() for f in focus_list]
     columns = st.columns(len(focus_list))
@@ -67,39 +70,49 @@ def focus_tags_disp(focus_list):
             unsafe_allow_html=True
         )
 
-## INITIALIZE session state variables
+########################################### INITIALIZE session state variables #####################################################
+# these are reset with each interview simulater restart and each new session
 if "stage" not in st.session_state:
+    # contains state machine label
     st.session_state.stage = "upload"
 
+    # json string for resume output
     st.session_state.json_str = ""
 
+    # question storage variables 
     st.session_state.question = ""
     st.session_state.question_to_json = ""
 
+    # focus / topic lsit storage 
     st.session_state.focus = []
     st.session_state.topic_list = {area: 0 for area in FOCUS_AREAS}
 
+    # answer storage variables 
     st.session_state.answer = ""
     st.session_state.answer_to_json = ""
 
+    # feedback and score storage variables 
     st.session_state.feedback_to_json = ""
     st.session_state.scores = []
     st.session_state.focus_scores = []
     st.session_state.total_scores = []
 
+    # question hsitory to ensure no repeat experience questions
     st.session_state.q_history = []
 
+    # general history to allow for question navigation 
     st.session_state.history = []
-    st.session_state.pending = None
-    st.session_state.validation = {}
 
+    # question indexing variables
     st.session_state.total_q = 0
+    st.session_state.q_cur = 0 # describes current question view for navigation
 
-    st.session_state.q_cur = 0
 
+########################################################### FRONT END INITIALIZATION #######################################################
 st.title("Interview Simulator Practice")
 
-if st.session_state.total_q == 10:
+############## End of interview session summary page 
+if st.session_state.total_q == 10: # marks end of interview 
     st.title("You have completed the interview!")
     st.write("Here are your final performance statistics")
 
@@ -148,12 +161,10 @@ if st.session_state.total_q == 10:
         st.session_state.clear()
         st.rerun()
 
-## PRINT CONVO
-
-# start = st.session_state.q_cur * 4
-# end = start + 4
-
+########### In session page (default page)
 if len(st.session_state.history) != 0 and st.session_state.stage != "question":
+
+    # print conversation hsitory (refreshed with each state update so have to reprint)
     st.title("Question " + str(st.session_state.q_cur + 1))
     history = st.session_state.history[st.session_state.q_cur]
 
@@ -208,7 +219,7 @@ if len(st.session_state.history) != 0 and st.session_state.stage != "question":
                 if (item["role"]) == "user":
                     st.divider()
 
-## PAGE SET UP
+######## Side bar set up 
 with st.sidebar:
     col1, col2, col3 = st.columns(3)
 
@@ -240,7 +251,6 @@ st.sidebar.progress(percent)
 st.sidebar.title("Performance Stats: ")
 
 if len(st.session_state.total_scores) != 0:
-
     total_scores = st.session_state.total_scores[0:st.session_state.total_q + 1]
     avg = []
     for i in range(4):
@@ -261,7 +271,6 @@ else:
 st.sidebar.write("Total Score Average: " + str(int(total_avg)) + "/40")
 st.sidebar.progress(percent_2)
 
-
 with st.sidebar:
     cols1 = st.columns(2)
     for i in range(2):
@@ -280,7 +289,6 @@ with st.sidebar:
             percent = progress/10
             st.progress(percent)
 
-
 st.sidebar.title("Focus Area Progress: ")
 for topic, progress in st.session_state.topic_list.items():
     st.sidebar.write(topic)
@@ -288,16 +296,14 @@ for topic, progress in st.session_state.topic_list.items():
     if percent >= 1:
         percent = 0.999999
     st.sidebar.progress(percent)
-
-
-
     
-################################# STATES #######################################
+##################################################################### INTERVIEW STATES #############################################################
 
+# start of interivew resume upload state 
 if st.session_state.stage == "upload":
+
     print("file is being read...")
     file_object = st.file_uploader("Please select an input resume: ")
-    # file_path = sys.argv[1]
 
     file_path = ""
     if file_object is not None:
@@ -307,7 +313,6 @@ if st.session_state.stage == "upload":
             file_path = tmp.name
     
         if file_path is not None:
-
             parsed_text = parse_resume(file_path)
             st.write("Resume Successfully Uploaded!")
             time.sleep(1)
@@ -322,19 +327,16 @@ if st.session_state.stage == "upload":
             st.session_state.stage = "question"
             st.rerun()
 
+# if new question generation state 
 elif st.session_state.stage == "question":
-
+    # generate question
     question = generate_questions(st.session_state.json_str, st.session_state.topic_list, st.session_state.q_history)
-    
-    print(question)
 
     st.session_state.question_json = to_json(question)
     st.session_state.question = st.session_state.question_json["question"]
     st.session_state.focus = st.session_state.question_json["focus_areas"]
 
-    # st.write("DEBUG:", st.session_state.question_json)
-    # st.write("TYPE:", type(st.session_state.question_json))
-
+    #u update hsitory 
     st.session_state.history.append([{"role": "assistant", "content": st.session_state.question},
                                      {"role": "focus", "content": st.session_state.focus},
                                      {"role": "user", "content": None},
@@ -345,45 +347,50 @@ elif st.session_state.stage == "question":
     st.session_state.stage = "answer"
     st.rerun()
  
-
+# if gather user answer stage 
 elif st.session_state.stage == "answer":
-
-    if st.session_state.history[st.session_state.q_cur][2]["content"] is None:
+ 
+    if st.session_state.history[st.session_state.q_cur][2]["content"] is None: # ensures not revisiting question 
+        #retrieve user answer 
         if answer := st.chat_input("Provide your best answer to the following question:"):
             st.session_state.total_q += 1
             st.session_state.answer = answer
             st.session_state.answer_json = json.dumps(answer, indent=4)
 
-
-            
-            st.session_state.history[st.session_state.q_cur][2]["content"] = st.session_state.answer
-            
             for focus in st.session_state.focus:
                 if focus in st.session_state.topic_list:
                     if st.session_state.topic_list[focus] < 3:
                         st.session_state.topic_list[focus] += 1
-
+            
+            # update history 
+            st.session_state.history[st.session_state.q_cur][2]["content"] = st.session_state.answer
+            
             st.session_state.stage = "feedback"
             st.rerun()
 
-
+# if generate feedback stage
 elif st.session_state.stage == "feedback":
+    #generate feedback
     feedback = generate_feedback(st.session_state.answer_json, st.session_state.question_json, RUBRIC_AREAS, st.session_state.focus)
-    print(feedback)
     
+    # parse and format fedback 
     st.session_state.feedback_to_json = to_json(feedback)
     scores = st.session_state.feedback_to_json["scores"]
     st.session_state.total_scores.append(scores)
-    st.session_state.history[st.session_state.q_cur][3]["content"] = st.session_state.feedback_to_json
-    
-    # st.session_state.history.append({"role": "feedback", "content":st.feedback_to_json})
-    st.session_state.stage =  "wait"
 
+    # update history
+    st.session_state.history[st.session_state.q_cur][3]["content"] = st.session_state.feedback_to_json
+
+    # move to acknoledgment stage 
+    st.session_state.stage =  "wait"
     st.rerun()
 
-    
+
+# if in acknoledgment stage (time after feedback, waits until user is doen reviewing )  
 elif st.session_state.stage == "wait":
+    # move on button 
     if st.button("Got it!! Next Question!", use_container_width=True):
         st.session_state.stage = "question"
         st.session_state.q_cur += 1
+
         st.rerun()
